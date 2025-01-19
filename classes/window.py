e9969da069272f04b52937db11540527bdf39166
Quera222 <103673118+Quera222@users.noreply.github.com>
2025-01-19 17:43:44 +0100

    front corrected

--- classes/window.py
+++ classes/window.py
@@ -7,6 +7,7 @@ class window:
                 self.okno.geometry("700x400")
                 self.list=lista
                 self.i=0
+                self.punkty=0
                 self.tresc_pytania = None
                 self.odp = []
                 self.budujokno()
@@ -14,12 +15,12 @@ class window:
 
 
         def budujokno(self):
-                #self.okno.grid_columnconfigure(0, weight=1) 
                 self.etykieta1=Label(text="Test z informatyki", font=("Arial", 20, "bold"))
-                self.etykieta1.grid(row=0,column=0, sticky="n") 
-                self.przycisk1=Button(width=10, text="Rozpocznij", command=self.budujpytania)
-                self.przycisk1.grid(row=1,column=0, sticky="N")
-                self.przycisk2=Button(width=10, text="Wyjdź", command=self.okno.quit)
+                self.etykieta1.pack(padx=10, pady=10)
+                self.przycisk1=Button(width=10, text="Rozpocznij", command=self.budujpytania, bg="#93bf85")
+                self.przycisk1.pack(padx=10, pady=10)
+                self.przycisk2=Button(width=10, text="Wyjdź", command=self.okno.quit, bg="#ff5656")
+                self.przycisk2.pack()
 
         def budujpytania(self):
                         self.etykieta1.destroy()
@@ -28,29 +29,52 @@ class window:
 
                         x=IntVar()
                         x.set(0)
-
+                        self.okno.columnconfigure(0, weight=1)
+                        self.okno.columnconfigure(1, weight=1)
                         self.etykietaKtorePytanie=Label(text=f"Pytanie {self.i+1} z {len(self.list)}")
-                        self.etykietaKtorePytanie.grid(row=2,column=2, sticky="N")
+                        self.etykietaKtorePytanie.grid(row=3,column=1, sticky="WN")
+
+                        self.frame=Frame(self.okno, width=200)  # ramka na odpowiedzi
+                        self.frame.grid(row=1,column=0, sticky="N", rowspan=3)
+
 
                         self.budujWidokPytania()
 
-                        self.przyciskDalej=Button(width=10, text="Dalej", command=self.budujWidokPytania)
-                        self.przyciskDalej.grid(row=0,column=2, sticky="N")
-                        self.przyciskWyjdz=Button(width=10, text="Wyjdź", command=self.okno.quit)
-                        self.przyciskWyjdz.grid(row=1,column=2, sticky="N")
+
+                        #przycisk DALEJ
+                        self.przyciskDalej=Button(width=10, text="Dalej", command=self.budujWidokPytania, bg="#88b7d5")
+                        self.przyciskDalej.grid(row=1,column=1, sticky="WN", pady=10)
+                        #przycisk WYJDŹ
+                        self.przyciskWyjdz=Button(width=10, text="Wyjdź", command=self.okno.quit, bg="#ff5656")
+                        self.przyciskWyjdz.grid(row=2,column=1, sticky="WN", pady=10)
      
 
         def budujEkranKoncowy(self):
-                self.etykietaKoniec = Label(text="Koniec quizu", font=("Arial", 20, "bold"))
-                self.etykietaKoniec.grid(row=0,column=0, sticky="N")
+                #niszczenie poprzednich widgetów
                 self.tresc_pytania.destroy()
                 self.przyciskDalej.destroy()
                 self.przyciskWyjdz.destroy()
+                self.etykietaKtorePytanie.destroy()
+                self.frame.destroy()
                 for i in range(0, len(self.odp)):
                         self.odp[i].destroy()
                 self.odp=[]
-                self.przyciskWyjdz=Button(width=10, text="Wyjdź", command=self.okno.quit)
-                self.przyciskWyjdz.grid(row=1,column=2, sticky="N")
+
+                #budowanie ekranu końcowego
+                self.etykietaKoniec = Label(text="Koniec quizu", font=("Arial", 20, "bold"))
+                self.etykietaKoniec.grid(row=0,column=0, sticky="N", pady=10)
+
+                self.etykietaKoniec2 = Label(text="Dziękujemy za udział w teście", font=("Arial", 10))
+                self.etykietaKoniec2.grid(row=1,column=0, sticky="N", pady=10)
+
+                self.etykietaIlePunktow = Label(text=f"Ilość punktów: {self.punkty}", font=("Arial", 10))
+                self.etykietaIlePunktow.grid(row=2,column=0, sticky="N", pady=10)
+
+                self.etykietaOcena = Label(text="Ocena: 6", font=("Arial", 10, "bold"))
+                self.etykietaOcena.grid(row=3,column=0, sticky="N", pady=10)
+
+                self.przyciskWyjdz=Button(width=10, text="Wyjdź", command=self.okno.quit, bg="#ff5656")
+                self.przyciskWyjdz.grid(row=4,column=0, sticky="N", pady=10)
 
 
         def budujWidokPytania(self):
@@ -65,11 +89,11 @@ class window:
                        
                         self.etykietaKtorePytanie.config(text=f"Pytanie {self.i+1} z {len(self.list)}")
                         self.tresc_pytania=Label(text=self.list[self.i].text, font=("Arial", 20, "bold"))
-                        self.tresc_pytania.grid(row=0,column=0, sticky="N")
+                        self.tresc_pytania.grid(row=0,column=0, sticky="N", pady=15)
 
                         for j in range(0, len(self.list[self.i].answers)):
-                               radiobutton = Radiobutton(text=self.list[self.i].answers[j].text, value=j)
-                               radiobutton.grid(row=j + 1, column=0, sticky="N")
+                               radiobutton = Radiobutton(self.frame, text=self.list[self.i].answers[j].text, value=j, anchor="w", padx=30)
+                               radiobutton.grid(row=j + 1, column=0, sticky="SENW")
                                self.odp.append(radiobutton)  # Dodawanie do listy odp
                 elif self.i==len(self.list):
                         self.budujEkranKoncowy()
--- test.py
+++ test.py
@@ -11,6 +11,7 @@ a4= Answer("Stest","stoks3")
 
 q1= Question("Qtest",[a1,a2,a3,a4],[a1,a2])
 
+#tutaj musi być własna ścieżka do pliku aby działało
 list = readQuestions("C:/Users/quera/OneDrive/Pulpit/studia/semestr5/Python/Projekt/sggw-python-quiz/questions/question.txt")
 print(list[0].text)
 print(q1.text)
